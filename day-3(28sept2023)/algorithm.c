ad37c90220b4880418f3dfcdc775422fc2382d73
Alok-Sci <alokscientist7905@gmail.com>
2023-09-29 00:40:39 +0530

    Day-3 at SPI

---
+++ day-3(28sept2023)/algorithm.c
@@ -0,0 +1,11 @@
+/*
+
+--Algorithm--
+): It is a concept of writing a program, step-by-step to perform a specific task.
+
+1. declare header files
+2. create the main function
+3. declare a variable
+4. take input from user
+
+*/
